ba665b472db6f11bd85f4ba0d0a17d0a49f6f22b
Huyhocdev <163633588+Huyhocdev@users.noreply.github.com>
2024-06-15 22:28:05 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Huỳn';
+const giftUrl = 'https://www.facebook.com/huy.vugia.92560/';
+const eventName = 'Gửi chị';
+const titleCard = 'Tặng Emhuỳn';
+const contentCard = 'Chị ơi, gặp được chị trong thế giới này là một điều may mắn đối với em vì thế giới này rộng lắm, gặp được chị, nếu có thể chị là một phần trong cuộc sống của em nhé .';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
